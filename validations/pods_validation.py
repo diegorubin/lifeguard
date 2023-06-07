cd186df3a95d5fdd53ff2573853996cd94b00609
Diego Rubin <contact@diegorubin.dev>
2023-06-07 16:54:20 -0300

    feat: using openai

--- validations/pods_validation.py
+++ validations/pods_validation.py
@@ -3,11 +3,12 @@ from lifeguard.actions.database import save_result_into_database
 from lifeguard.actions.notifications import notify_in_thread
 
 from lifeguard_k8s.validations.pods import pods_validation
+from lifeguard_openai.actions.errors import explain_error
 
 
 @validation(
     "diegorubindev-namespace-pods",
-    actions=[save_result_into_database, notify_in_thread],
+    actions=[explain_error, save_result_into_database, notify_in_thread],
     schedule={"every": {"minutes": 1}},
     settings={
         "notification": {
@@ -15,7 +16,7 @@ from lifeguard_k8s.validations.pods import pods_validation
             "template": """{%- if pods %}
 There are {{ pods | length }} pod(s) not running:
 {%- for pod in pods %}
-- {{ pod }}
+{{loop.index}}. {{ pod }}: {{ explanation[loop.index - 1] }}
 {%- endfor %}
 {% else %}
 All pods are running!
