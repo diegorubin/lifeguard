b5b305dd14167993cacd63c1f52e94b278236e7c
Diego Rubin <contact@diegorubin.dev>
2023-06-09 11:51:48 -0300

    feat: adding simple dashboard plugin

--- lifeguard_settings.py
+++ lifeguard_settings.py
@@ -1,10 +1,11 @@
 import lifeguard_telegram
 import lifeguard_mongodb
+import lifeguard_simple_dashboard
 
 from lifeguard.auth import BASIC_AUTH_METHOD
 from custom_settings import API_USERNAME, API_PASSWORD
 
-PLUGINS = [lifeguard_telegram, lifeguard_mongodb]
+PLUGINS = [lifeguard_telegram, lifeguard_mongodb, lifeguard_simple_dashboard]
 
 
 def setup(lifeguard_context):
