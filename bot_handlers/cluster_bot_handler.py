e60887c0ed6a125f8c100ade4305ada867962e20
Diego Rubin <contact@diegorubin.dev>
2023-06-16 15:20:59 -0300

    feat: poc with AI and K8S cluster

---
+++ bot_handlers/cluster_bot_handler.py
@@ -0,0 +1,97 @@
+import re
+import yaml
+
+from os.path import join, abspath, dirname
+
+from lifeguard.logger import lifeguard_logger as logger
+from lifeguard_telegram.bot import bot_handler
+
+from lifeguard_k8s.infrastructure.pods import (
+    get_namespace_infos,
+    delete_a_pod,
+    get_last_error_event_from_pod,
+)
+from lifeguard_openai.infrastructure.prompt import execute_prompt
+
+from custom_settings import BOT_VALID_CHATS, PROMPT_LANG
+
+
+current_directory = dirname(abspath(__file__))
+with open(join(current_directory, "cluster.yaml")) as file:
+    CONTEXT = yaml.load(file, Loader=yaml.FullLoader)
+
+
+def valid_request(update):
+    context = update.message.to_dict()
+    logger.info("chat id: %s", context["chat"]["id"])
+    if str(context["chat"]["id"]) not in BOT_VALID_CHATS:
+        logger.warn("unauthorized request %s", context)
+        return False
+    return True
+
+
+def restart_pod(update, pod_name):
+    logger.info("restart pod %s", pod_name)
+    delete_a_pod("diegorubindev", pod_name)
+    update.message.reply_text(f"reiniciando pod {pod_name}")
+
+
+def explain_pod_error(update, pod_name):
+    logger.info("explain pod error %s", pod_name)
+    response = execute_prompt(
+        f"""Dado o erro abaixo, explique o motivo, por favor:
+{get_last_error_event_from_pod("diegorubindev", pod_name)}
+        """
+    )
+    update.message.reply_text(response)
+
+
+def _execute_commands(update, response):
+    for command in CONTEXT["commands"]:
+        if command["command"] in response:
+            logger.info("executing command %s", command["command"])
+            regex = re.compile(f"{command['command']} (.*)")
+            arg = regex.search(response).group(1)
+            function = globals()[command["command"].replace(" ", "_")]
+            function(update, arg)
+            return True
+    return False
+
+
+def _build_infos(namespace_infos):
+    infos = ["nome,status,restarts"]
+    for pod in namespace_infos["pods"]:
+        infos.append(
+            f"{pod['name']},{pod['status']},{pod['containers'][0]['restart_count']}"
+        )
+
+    return "\n".join(infos)
+
+
+def _build_commands(commands):
+    return "\n\n".join([command["definitions"][PROMPT_LANG] for command in commands])
+
+
+@bot_handler("cluster")
+def cluster(update, _context):
+    if not valid_request(update):
+        return
+    question = update.message.to_dict()["text"].replace("/cluster ", "")
+
+    namespace_infos = get_namespace_infos("diegorubindev")
+
+    prompt = (
+        CONTEXT["prompt_template"][PROMPT_LANG]
+        .replace("INFOS", _build_infos(namespace_infos))
+        .replace("COMMANDS", _build_commands(CONTEXT["commands"]))
+        .replace("QUESTION", question)
+    )
+
+    logger.info("prompt: %s", prompt)
+
+    response = execute_prompt(prompt)
+
+    logger.info("response: %s", response)
+
+    if not _execute_commands(update, response):
+        update.message.reply_text(response)
--- custom_settings.py
+++ custom_settings.py
@@ -19,6 +19,15 @@ settings_manager = SettingsManager(
             "default": "",
             "description": "Password to access lifeguard",
         },
+        "BOT_VALID_CHATS": {
+            "default": "",
+            "type": "list",
+            "description": "Valid chats to access bot",
+        },
+        "PROMPT_LANG": {
+            "default": "pt_br",
+            "description": "Language to execute prompt",
+        },
     }
 )
 
@@ -31,3 +40,7 @@ STATUS_NOTIFICATION_SERVICE_API_TOKEN = settings_manager.read_value(
 
 API_USERNAME = settings_manager.read_value("API_USERNAME")
 API_PASSWORD = settings_manager.read_value("API_PASSWORD")
+
+BOT_VALID_CHATS = settings_manager.read_value("BOT_VALID_CHATS")
+
+PROMPT_LANG = settings_manager.read_value("PROMPT_LANG")
