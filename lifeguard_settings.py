8e8b0d07a83aa460c184d160829a811d4f2f14a8
Diego Rubin <contact@diegorubin.dev>
2025-01-02 09:20:13 -0300

    chore: pods validation

--- lifeguard_settings.py
+++ lifeguard_settings.py
@@ -11,4 +11,3 @@ PLUGINS = [lifeguard_mongodb, lifeguard_telegram, lifeguard_simple_dashboard]
 def setup(lifeguard_context):
     lifeguard_context.auth_method = BASIC_AUTH_METHOD
     lifeguard_context.users = [{"username": API_USERNAME, "password": API_PASSWORD}]
-    # lifeguard_context.cors_settings = {r"/*": {"origins": "*"}}
